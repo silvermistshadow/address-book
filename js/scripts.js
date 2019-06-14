507fbc0fb97a582ab426e397f4a345599e04804a
silvermistshadow <bmead72@gmail.com>
2019-06-14 12:47:29 -0700

    initial setup, no UI yet

---
+++ js/scripts.js
@@ -0,0 +1,57 @@
+// Business Logic for AddressBook ---------
+function AddressBook() {
+  this.contacts = [],
+  this.currentId = 0
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();   // <--- This line is new!
+  this.contacts.push(contact);
+}
+
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+AddressBook.prototype.findContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {     // <-- This line is new!
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }                          // <-- This line is also new!
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {     // <-- This line is new!
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }                          // <-- This line is also new!
+  };
+  return false;
+}
+
+// Business Logic for Contacts ---------
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+
+
+
+
+
+
+//User interface
